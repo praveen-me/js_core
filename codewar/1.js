0537720387a58d1b09483a894770690c1a8f11b6
praveen <praveenkrsaini0@gmail.com>
2019-04-24 23:25:47 +0530

    Add codewars challenge

---
+++ codewar/1.js
@@ -0,0 +1,24 @@
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+
+// likes [] // must be "no one likes this"
+// likes ["Peter"] // must be "Peter likes this"
+// likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+    const array1 = [names[0], names[1]]
+    
+    if(names.length === 0) {
+        return "no one likes this"
+    } else if (names.length === 1) {
+        return `${names[0]} likes this`
+    } else if(names.length <= 3) {
+        return `${names.length === 2 ? array1.join(' and ') : array1.join(', ') } ${names.length === 2 ? '' : `and ${names[names.length - 1]} `}like this`;
+    } else if(names.length >= 4) {
+    console.log(names.length)
+        return `${array1.join(', ')} and ${names.length - 2} others like this`;
+    }
+}
