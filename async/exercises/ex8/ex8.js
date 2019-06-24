6367f99628a7bef97799eba526514c86de78a5e0
praveen-me <praveenkrsaini0@gmail.com>
2019-06-24 17:52:54 +0530

    Added solution of ex8 in async

--- async/exercises/ex8/ex8.js
+++ async/exercises/ex8/ex8.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
 	var $btn = $("#btn"),
-		$list = $("#list");
+		$list = $("#list"),
+		clicks = ASQ.react.of(),
+		msgs = ASQ.react.of(),
+		latest;
 
 	$btn.click(function(evt){
 		// TODO
+		clicks.push(evt);
+	});
+
+	setInterval(function() {
+		if(latest) {
+			msgs.push('clicked');
+			latest = null;
+		}
+	}, 1000);
+
+	clicks.val(function(evt) {
+		latest = evt;
 	});
 
 	// TODO: setup sampled sequence, populate $list
+	msgs.val(function(msg) {
+		$list.append(`<div>${msg}</div>`)
+	})
 });
